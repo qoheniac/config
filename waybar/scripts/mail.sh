d2ef59d673f51d8465f4440421d4b44c6ea9cab9
qoheniac <29815186+qoheniac@users.noreply.github.com>
2021-01-22 11:18:32 +0100

    display when mails cannot be checked (yet)

--- waybar/scripts/mail.sh
+++ waybar/scripts/mail.sh
@@ -2,9 +2,16 @@
 # personalmail and workmail are defined in /etc/hosts with credentials in ~/.netrc
 PERSONAL="$(curl -sfnX "STATUS INBOX (UNSEEN)" imap://personalmail:1143/INBOX | tr -dc "[:digit:]")"
 WORK="$(curl -sfnkX "STATUS INBOX (UNSEEN)" imaps://workmail/INBOX | tr -dc "[:digit:]")"
-COUNT="$(echo "$PERSONAL+$WORK" | bc)"
-if [ "$COUNT" -eq 0 ]; then
-    echo ""
+is_int () {
+    [ "$1" -ge 0 ] 2> /dev/null
+}
+if is_int "$PERSONAL" && is_int "$WORK"; then
+    COUNT="$(echo "$PERSONAL+$WORK" | bc)"
+    if [ "$COUNT" -eq 0 ]; then
+        echo ""
+    else
+        echo " $COUNT"
+    fi
 else
-    echo " $COUNT"
+    echo " ?"
 fi
