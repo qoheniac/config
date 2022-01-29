b12b1dfa91f1f7b962b4c1794a3f2a2fe6a6cada
qoheniac <29815186+qoheniac@users.noreply.github.com>
2022-01-29 11:48:25 +0100

    bug fixes

--- waybar/scripts/weather.sh
+++ waybar/scripts/weather.sh
@@ -12,13 +12,13 @@ LOC=""
 REQUEST_GEO="$(wget -qO - http://openwifi.su/api/v1/bssids/"$BSSIDS")"
 if [ "$(jq ".count_results" <<< "$REQUEST_GEO")" -gt 0 ]; then
     LAT="$(jq ".lat" <<< "$REQUEST_GEO")"
-    LAT="$(jq ".lat" <<< "$REQUEST_GEO")"
+    LON="$(jq ".lon" <<< "$REQUEST_GEO")"
     LOC="$LAT,$LON"
 fi
 
 # get weather information
-text="$(curl -s 'https://wttr.in/$LOC?format=1')"
-tooltip="$(curl -s 'https://wttr.in/$LOC?0QT' |
+text="$(curl -s "https://wttr.in/$LOC?format=1")"
+tooltip="$(curl -s "https://wttr.in/$LOC?0QT" |
     sed 's/\\/\\\\/g' |
     sed ':a;N;$!ba;s/\n/\\n/g' |
     sed 's/"/\\"/g')"
