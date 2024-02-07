1c7046dfa845f1e164b28b61951e2eb11f427b04
qoheniac <29815186+qoheniac@users.noreply.github.com>
2024-02-07 13:58:45 +0100

    use apple_bssid_locator instead of discontinued OpenWifi.su

--- waybar/scripts/weather.sh
+++ waybar/scripts/weather.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 # weather information for location based on seen WiFi BSSIDs or IP as fallback
 
-# get close WiFi BSSIDs
-BSSIDS="$(nmcli device wifi list |
-    awk 'NR>1 {if ($1 != "*") {print $1}}' |
-    tr -d ":" |
-    tr "\n" ",")"
-
-# get geographical location
-LOC=""
-REQUEST_GEO="$(wget -qO - http://openwifi.su/api/v1/bssids/"$BSSIDS")"
-if [ "$(jq ".count_results" <<< "$REQUEST_GEO")" -gt 0 ]; then
-    LAT="$(jq ".lat" <<< "$REQUEST_GEO")"
-    LON="$(jq ".lon" <<< "$REQUEST_GEO")"
-    LOC="$LAT,$LON"
-fi
+# get geographical location from close WiFi BSSIDs
+for BSSID in $(nmcli device wifi list | awk 'NR>1 && NR<12 {if ($1 != "*") {print $1}}'); do
+    # https://github.com/darkosancanin/apple_bssid_locator
+    LOC="$(apple_bssid_locator "$BSSID" 2> /dev/null | tr -d '\n' |
+        grep -Eo 'Latitude: [0-9.]+Longitude: [0-9.]+' |
+        sed 's/Latitude: //' | sed 's/Longitude://')"
+    if [ -n "$LOC" ]; then
+        LOC="$(LC_ALL=C printf "%.2f,%.2f" $LOC)"
+        break;
+    fi
+done
 
 # get weather information
 text="$(curl -s "https://wttr.in/$LOC?format=1")"
@@ -24,6 +21,6 @@ tooltip="$(curl -s "https://wttr.in/$LOC?0QT" |
     sed 's/"/\\"/g')"
 
 # output for Waybar
-if ! grep -q "Unknown location" <<< "$text"; then
+if ! echo "$text" | grep -q "Unknown location"; then
     echo "{\"text\": \"$text\", \"tooltip\": \"<tt>$tooltip</tt>\", \"class\": \"weather\"}"
 fi
